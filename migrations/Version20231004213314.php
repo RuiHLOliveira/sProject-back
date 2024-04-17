e1f5fe3de45921a5a8cc8424e4bd8954a888ec60
Rui H L Oliveira <ruihloliveira@gmail.com>
2024-04-17 14:16:27 -0300

    adjust1.6

--- migrations/Version20231004213314.php
+++ migrations/Version20231004213314.php
@@ -20,7 +20,7 @@ final class Version20231004213314 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE configuracao_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "configuracao_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE configuracao (id INT NOT NULL, usuario_id INT NOT NULL, chave VARCHAR(255) NOT NULL, valor VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_36CAF392DB38439E ON configuracao (usuario_id)');
         $this->addSql('COMMENT ON COLUMN configuracao.created_at IS \'(DC2Type:datetime_immutable)\'');
@@ -33,7 +33,7 @@ final class Version20231004213314 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE configuracao_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "configuracao_id_seq" CASCADE');
         $this->addSql('ALTER TABLE configuracao DROP CONSTRAINT FK_36CAF392DB38439E');
         $this->addSql('DROP TABLE configuracao');
     }
