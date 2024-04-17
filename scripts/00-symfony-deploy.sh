8ad19cbd1c6e0168d2b879fb5aaa6e0ae9647e73
Rui H L Oliveira <ruihloliveira@gmail.com>
2024-04-17 13:53:45 -0300

    adjust1.4

--- scripts/00-symfony-deploy.sh
+++ scripts/00-symfony-deploy.sh
@@ -5,8 +5,8 @@ composer install --no-dev --optimize-autoloader
 echo "Caching config..."
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 
-echo "creating database..."
-bin/console doctrine:database:create --if-not-exists
+# echo "creating database..."
+# bin/console doctrine:database:create --if-not-exists
 
 echo "migrating..."
 bin/console doctrine:migrations:migrate
